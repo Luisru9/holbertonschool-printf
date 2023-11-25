5a654b16d073d55a64f2b1c2ab33b4eada1f2e4f
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-11-25 08:55:34 -0600

    printf-work

--- functions.c
+++ functions.c
@@ -10,10 +10,9 @@
 
 int print_c(va_list args)
 {
-	int c;
-	c = va_arg(args, int);
-
-	return (_putchar(c));
+int c;
+c = va_arg(args, int);
+return (_putchar(c));
 }
 
 /**
@@ -24,20 +23,20 @@ int print_c(va_list args)
 
 int print_s(va_list args)
 {
-	int i, count = 0;
-	char *str;
+int i, count = 0;
+char *str;
 
-	i = 0;
-	str = va_arg(args, char*);
-	if (str == NULL)
-		str = "(null)";
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-		count++;
-	}
-	return (count);
+i = 0;
+str = va_arg(args, char*);
+if (str == NULL)
+str = "(null)";
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+count++;
+}
+return (count);
 }
 
 /**
@@ -49,14 +48,14 @@ int print_s(va_list args)
 
 int print_percent(va_list args)
 {
-	char *str;
+char *str;
 
-	str = "%";
-	if (va_arg(args, int) == *str)
-	{
-		return (*str);
-	}
-	return (*str);
+str = "%";
+if (va_arg(args, int) == *str)
+{
+return (*str);
+}
+return (*str);
 }
 
 /**
@@ -67,31 +66,31 @@ int print_percent(va_list args)
 
 int print_d(va_list args)
 {
-	unsigned int absolute, aux, countnum, count;
-	int n;
+unsigned int absolute, aux, countnum, count;
+int n;
 
-	count = 0;
-	n = va_arg(args, int);
-	if (n < 0)
-	{
-		absolute = (n * -1);
-		count += _putchar('-');
-	}
-	else
-		absolute = n;
-	aux = absolute;
-	countnum = 1;
-	while (aux > 9)
-	{
-		aux /= 10;
-		countnum *= 10;
-	}
-	while (countnum >= 1)
-	{
-		count += _putchar(((absolute / countnum) % 10) + '0');
-		countnum /= 10;
-	}
-	return (count);
+count = 0;
+n = va_arg(args, int);
+if (n < 0)
+{
+absolute = (n * -1);
+count += _putchar('-');
+}
+else
+absolute = n;
+aux = absolute;
+countnum = 1;
+while (aux > 9)
+{
+aux /= 10;
+countnum *= 10;
+}
+while (countnum >= 1)
+{
+count += _putchar(((absolute / countnum) % 10) + '0');
+countnum /= 10;
+}
+return (count);
 }
 
 /**
@@ -102,5 +101,5 @@ int print_d(va_list args)
 
 int print_i(va_list args)
 {
-	return (print_d(args));
+return (print_d(args));
 }
